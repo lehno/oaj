8c5513f17bb9327d5399d4702484922ad94ce8fc
Leandro Hoffmann <leandro.hoffmann@vejoaovivo.com.br>
2015-03-13 00:36:03 -0300

    Many fixes on mongo, music player working

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,4 +1,9 @@
 'use strict';
+var mongoose = require('mongoose'),
+    Grid = require('gridfs-stream');
+    Grid.mongo = mongoose.mongo;
+
+var gfs = new Grid(mongoose.connection.db);
 
 /**
  * Module dependencies.
@@ -8,4 +13,33 @@ exports.index = function(req, res) {
 		user: req.user || null,
 		request: req
 	});
+};
+
+exports.getFile = function (req, res) {
+    gfs.files.find({_id: mongoose.Types.ObjectId(req.query.file)}).toArray(function (err, files) {
+        if (files.length === 0) {
+            return res.status(400).send({
+                message: 'File not found'
+            });
+        }
+
+        res.writeHead(200, {'Content-Type': files[0].contentType});
+
+        var readstream = gfs.createReadStream({
+            filename: files[0].filename
+        });
+
+        readstream.on('data', function (data) {
+            res.write(data);
+        });
+
+        readstream.on('end', function () {
+            res.end();
+        });
+
+        readstream.on('error', function (err) {
+            console.log('An error occurred!', err);
+            throw err;
+        });
+    });
 };
